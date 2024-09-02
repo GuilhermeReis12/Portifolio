dc40e8d29587d78e69329e2b284bfa1452211bb0
GuilhermeReis12 <guilhermeoliveira12102001@gmail.com>
2024-09-02 19:24:45 -0300

    melhorias no front end

--- meu-portifolio/src/routes/AppRoutes.jsx
+++ meu-portifolio/src/routes/AppRoutes.jsx
@@ -9,6 +9,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/Portifolio/" element={<Home />} />
        <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/contact" element={<Contact />} /> 
